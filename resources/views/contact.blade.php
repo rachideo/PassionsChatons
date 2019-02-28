320b6d7691fe4102f7bfd4223cf082ad2a9eb961
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-02-28 09:54:07 +0100

    Pareil que commit d'avant mais avec page contact en plus

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,7 @@
 @extends('layout')
+
 @section('title', 'Contact')
+
 @section('content')
-
-    <h1> Contact </h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
-
 @endsection
