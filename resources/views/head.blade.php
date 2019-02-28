57896768c15670db3d9816a2701c99108eebbb62
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-02-28 13:23:09 +0100

    Réparation de mes conneries

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
 </head>
