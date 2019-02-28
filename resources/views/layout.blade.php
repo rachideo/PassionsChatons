a5e49af436b34f95757bb502b0d64a1d3d31282b
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-02-28 10:08:50 +0100

    Découpage du layout principal avec 3 nouveaux éléments : navbar, head et scripts. Ces éléments sont liés au main avec @include

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,35 +1,12 @@
 <!doctype html>
+
 <html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
+
+@include('head')
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light header justify-content-between">
-    <a href="/" class="navbar-brand">
-        <h1 class="mx-2 nav-title">passionChaton.love</h1>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/liste-produits">Cat-alogue</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/panier">Mon panier</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+@include('navbar')
 
 <div class="container">
     <div class="titre my-md-4">
@@ -43,8 +20,6 @@
 
 </body>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+@include('scripts')
 
 </html>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light header justify-content-between">
+    <a href="/" class="navbar-brand">
+        <h1 class="mx-2 nav-title">passionChaton.love</h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/liste-produits">Cat-alogue</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/panier">Mon panier</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/contact">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
