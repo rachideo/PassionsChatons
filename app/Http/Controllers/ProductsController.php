c37aa8776b9944d23495d80ba5c3c524e3b5c641
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-02-28 16:21:29 +0100

    Modification de ProductController pour pouvoir passer les données de mon tableau d'articles à la vue product-details

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,10 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProductsController extends Controller
-{
+class ProductsController extends Controller {
 
     public function show($product) {
-        return view('product-details')->with('articleName', $product);
+
+        $chatons = [
+            [
+                'nom' => "Fiddle",
+                'prix' => 399,
+                'photo' => "images/Fiddle.jpg",
+            ],
+            [
+                'nom' => "Mitten",
+                'prix' => 289,
+                'photo' => "images/Mitten.jpg",
+            ],
+            [
+                'nom' => "Strawberry",
+                'prix' => 599,
+                'photo' => "images/Strawberry.jpg",
+            ]
+        ];
+        return view('product-details')->with('articleDetails', $chatons[$product]);
     }
 }
--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -3,5 +3,8 @@
 @section('title', 'Détail produit')
 
 @section('content')
+    {{ $articleDetails['nom'] }}<br>
+    {{ $articleDetails['prix'] }}<br>
+    {{ $articleDetails['photo'] }}
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
 @endsection
