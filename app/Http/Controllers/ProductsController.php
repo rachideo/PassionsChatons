5959de15a1dc536820e5c10a8f83e8e35608f955
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-02-28 16:26:53 +0100

    On peut trouver maintenant les fiches produits en tappant le nom des chatons dans l'url

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,22 +9,23 @@ class ProductsController extends Controller {
     public function show($product) {
 
         $chatons = [
-            [
+            'fiddle' => [
                 'nom' => "Fiddle",
                 'prix' => 399,
                 'photo' => "images/Fiddle.jpg",
             ],
-            [
+            'mitten' => [
                 'nom' => "Mitten",
                 'prix' => 289,
                 'photo' => "images/Mitten.jpg",
             ],
-            [
+            'strawberry' => [
                 'nom' => "Strawberry",
                 'prix' => 599,
                 'photo' => "images/Strawberry.jpg",
             ]
         ];
+        
         return view('product-details')->with('articleDetails', $chatons[$product]);
     }
 }
