7e8fb7deb47b9da9ef22d50116ab2ad8fdf3504a
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-02-28 11:30:13 +0100

    Déplacement du fichier css dans les ressources et modification de app.scss, création d'un dossier images dans les ressources pour que les fichiers puissent être compilés. Remplacement des liens vers feuilles de style par un lien asset dans le layout.

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,6 +2,5 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
