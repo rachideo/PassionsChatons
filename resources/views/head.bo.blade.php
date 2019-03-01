94faf5f99ba4ab6b07391753462f4677fe6c18b5
Rachid El Mahdali <rachid.el-mahdali@Digital-Grenoble.local>
2019-03-01 16:05:36 +0100

    Nouvelle feuille de style pour BackOffice et nouvau head pour le layout BackOffice

---
+++ resources/views/head.bo.blade.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/style_backoffice.css') }}" crossorigin="anonymous">
+</head>
