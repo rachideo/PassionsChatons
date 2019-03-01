35428ec912095d4b27405e7e603686ad2789e15b
CalMaguire <callista.maguire@le-campus-numerique.fr>
2019-03-01 14:05:23 +0100

    Added a font to use in <p> and changed product detail display

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -1,12 +1,12 @@
 @extends('layout')
 
-@section('title', 'Détail produit')
+@section('title', $articleDetails['nom'])
 
 @section('content')
 
-    <img src ="{{ asset ($articleDetails['photo']) }}" class="rounded mx-auto d-block" >
-    {{ $articleDetails['nom'] }}<br>
-    {{ $articleDetails['prix'] }}<br>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
+    <img src ="{{ asset ($articleDetails['photo']) }}" class="rounded mx-auto d-block" > <br>
+     <div class="container-fluid bg-3 text-center">
+        <h3>Price : {{ $articleDetails['prix'] }} </h3>
+        </div><br>
+    <p class="container-fluid bg-3 text-center" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
 @endsection
