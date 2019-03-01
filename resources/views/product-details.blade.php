af9d680eb8d19ac76bcfef3d8dcb60f032a491bc
CalMaguire <callista.maguire@le-campus-numerique.fr>
2019-03-01 12:04:45 +0100

    Changes to product-details and Css

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -3,8 +3,11 @@
 @section('title', 'Détail produit')
 
 @section('content')
+    <div  class ="myBorder">
+    <img src ="{{ asset ($articleDetails['photo']) }}" class="rounded mx-auto d-block">
+    </div>
     {{ $articleDetails['nom'] }}<br>
     {{ $articleDetails['prix'] }}<br>
-    {{ $articleDetails['photo'] }}
+
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
 @endsection
