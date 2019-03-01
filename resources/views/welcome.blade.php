1a38a6d37030ccce48947793e704a21360483aba
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-03-01 16:04:02 +0100

    Intégration page d'accueil image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <img src="{{ asset ('images/welcomeChat.png') }}">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
+    <div class="row align-items-center">
+        <div class="col">
+            <img class="" src="{{ asset ('images/welcomeChat.png') }}">
+        </div>
+        <div class="col">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet atque blanditiis cum doloribus esse hic incidunt inventore ipsa minus nihil numquam, optio pariatur qui recusandae rem repudiandae tenetur, ut.</p>
+        </div>
+    </div>
 @endsection
