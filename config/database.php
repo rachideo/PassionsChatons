e3d254ca58f177f19bfc785f72429a6ecf757e9b
Bastien Joffre <bastien.joffre@Digital-Grenoble.local>
2019-03-18 09:43:10 +0100

    Correction config/database

--- config/database.php
+++ config/database.php
@@ -42,13 +42,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'passionchatons',
-            'username' => 'admin',
-            'password' => 'etugre2020',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
