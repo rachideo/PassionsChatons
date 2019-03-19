bc190f136509b78518c6ebcc90944136484858d3
CalMaguire <callista.maguire@le-campus-numerique.fr>
2019-03-19 10:30:50 +0100

    fixed padding and margin for sign in page

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class = "container ">
         <div class="row">
-            <div class="col myForms">
+            <div class="col-md myForms p-4 m-4">
                 <h3> Identifiez vous</h3> <br>
                     <form method="GET" action="{{route('account')}}">
                         <div class="form-group">
@@ -20,18 +20,18 @@
                     </form>
             </div>
 
-            <div class="col myForms">
-                <h3> Créer compte</h3> <br>
+            <div class="col-md myForms p-4 m-4">
+                <h3>Créer compte</h3> <br>
                     <form>
                         <div class="form-group">
                             <label for="InputPrenom"> <h4>Prénom</h4></label>
                             <input type="text" class="form-control" id="InputPrenom" aria-describedby="emailHelp" placeholder="Votre prénom">
                         </div>
-                        <div class="form-group ">
+                        <div class="form-group">
                             <label for="InputNomFamille"> <h4>Nom de famille</h4></label>
                             <input type="text" class="form-control" id="InputNomFamille" aria-describedby="emailHelp" placeholder="Votre nom de famille">
                         </div>
-                        <div class="form-group ">
+                        <div class="form-group">
                             <label for="InputEmail"> <h4>Adresse email</h4></label>
                             <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Votre email">
                         </div>
