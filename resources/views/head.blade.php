54aa6b6c159b65cee6a3a62dba170a6d58051cbc
bastien-joffre <bastien.joffre@le-campus-numerique.fr>
2019-03-22 11:58:49 +0100

    petit ajout dans meta viewport : shrinktofit : no

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" crossorigin="anonymous">
