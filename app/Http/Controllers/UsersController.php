ae72c1d471f0343eee775040d76dba9e91faac04
CalMaguire <callista.maguire@le-campus-numerique.fr>
2019-03-28 16:23:10 +0100

    Added a few small changes to user controller and my account view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,25 +13,14 @@ class UsersController extends Controller
     }
 
     public function show() {
-//    $users = \App\User::all()->where('id','===', Auth::id());;
         $user = Auth::user();
-//        dd($user);
-    $orders = \App\Order::all()->where('user_id','===', Auth::id());;
+        $orders = \App\Order::all()->where('user_id','===', Auth::id());;
 
-        foreach ($orders as $order) {
-            foreach($order->products as $product){
-                $order->id;
-                $product->pivot->quantity;
-                $product->name;
-                $product->image;
-            }
-        }
          {
-//        return view('my-account')->with('users', $users)->with('orders', $orders)->with('product', $product);
+//
         return view('my-account', [
             'user' => $user,
             'orders' => $orders,
-            'product' => $product
         ]);
 
          }
